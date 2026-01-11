6446d8276cf286168a380a2ebd6c3f5c71100316
Adogo <gonda.ado@gmail.com>
2026-01-11 21:20:29 +0100

    fix: credit out of view

--- js/app.js
+++ js/app.js
@@ -1,66 +1,77 @@
-let canvas = document.getElementById('canvas');
-let credit = document.getElementById('credit');
-let ballCost = document.getElementById('ballCost');
-let numberOFAutoplay = document.getElementById('numberOFAutoplay');
-let toggleAutoPlay = document.getElementById('toggleAutoPlay');
-let riskInput = document.getElementById('riskInput');
-let rowsInput = document.getElementById('rowsInput')
+let canvas = document.getElementById("canvas");
+let credit = document.getElementById("credit");
+let ballCost = document.getElementById("ballCost");
+let numberOFAutoplay = document.getElementById("numberOFAutoplay");
+let toggleAutoPlay = document.getElementById("toggleAutoPlay");
+let riskInput = document.getElementById("riskInput");
+let rowsInput = document.getElementById("rowsInput");
 let ctx = canvas.getContext("2d");
-let game = new Plinko(800, 600, ctx, credit, ballCost, rowsInput, riskInput, false, numberOFAutoplay);
+let game = new Plinko(
+  canvas.clientWidth,
+  canvas.clientHeight,
+  ctx,
+  credit,
+  ballCost,
+  rowsInput,
+  riskInput,
+  false,
+  numberOFAutoplay
+);
 game.setPegs();
 game.setBaskets();
-credit.innerText = 'Credit: ' + game.total;
+credit.innerText = "Credit: " + game.total;
 
-document.addEventListener('keypress', spawnBall);
+console.log(canvas.clientWidth, canvas.clientHeight);
 
-toggleAutoPlay.addEventListener('click', autoplay);
+document.addEventListener("keypress", spawnBall);
 
-riskInput.addEventListener('change', setRisk)
+toggleAutoPlay.addEventListener("click", autoplay);
 
-rowsInput.addEventListener('change', setRows)
+riskInput.addEventListener("change", setRisk);
 
-function setRisk(){
-    if(game.balls.length != 0){
-        return;
-    }
-    game.risk = Number(riskInput.value);
-    game.setBaskets();
+rowsInput.addEventListener("change", setRows);
+
+function setRisk() {
+  if (game.balls.length != 0) {
+    return;
+  }
+  game.risk = Number(riskInput.value);
+  game.setBaskets();
 }
 
-function setRows(){
-    if(game.balls.length != 0){
-        return;
-    }
-    game.rows = Number(rowsInput.value);
-    game.setPegs();
-    game.setBaskets();
+function setRows() {
+  if (game.balls.length != 0) {
+    return;
+  }
+  game.rows = Number(rowsInput.value);
+  game.setPegs();
+  game.setBaskets();
 }
 
 setInterval(update, 1000 / 60);
 
-function update(){
-    game.draw();
+function update() {
+  game.draw();
 }
 
-function spawnBall(event){
-    if(event.code == 'Space' && game.autoplay == null){
-        game.spawnBall();
-    }
+function spawnBall(event) {
+  if (event.code == "Space" && game.autoplay == null) {
+    game.spawnBall();
+  }
 }
 
-function autoplay(){
-    if(game.autoplay == null){
-        toggleAutoPlay.innerText = '■';
-        game.autoplay = setInterval(startAutoplay, 1000)
-        ballCost.disabled = true;
-        numberOFAutoplay.disabled = true;
-    }
-    else{
-        toggleAutoPlay.innerText = '►';
-        game.resetAutoplay();
-    }
+function autoplay() {
+  if (game.autoplay == null) {
+    toggleAutoPlay.innerText = "■";
+    game.autoplay = setInterval(startAutoplay, 1000);
+    ballCost.disabled = true;
+    numberOFAutoplay.disabled = true;
+  } else {
+    toggleAutoPlay.innerText = "►";
+    game.resetAutoplay();
+  }
 }
 
-function startAutoplay(){
-    game.startAutoplay();
+function startAutoplay() {
+  game.startAutoplay();
 }
