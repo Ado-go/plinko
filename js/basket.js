a3ecf641ef081b5327c00d00154f2778afdd4026
Adogo <gonda.ado@gmail.com>
2026-01-11 21:39:24 +0100

    feat: basker color change

--- js/basket.js
+++ js/basket.js
@@ -1,24 +1,30 @@
-class Basket{
-    constructor(width, height, position, multiplier){
-        this.width = width;
-        this.height = height;
-        this.position = position;
-        this.multiplier = multiplier;
-        this.color = this.basketColor(multiplier);
-    }
+class Basket {
+  constructor(width, height, position, multiplier) {
+    this.width = width;
+    this.height = height;
+    this.position = position;
+    this.multiplier = multiplier;
+    this.color = this.basketColor(multiplier);
+  }
 
-    animateBasket(){
-        this.position.y += 2;
-        setTimeout(() => {
-            this.position.y -= 2;
-        }, 100);
+  animateBasket() {
+    this.position.y += 2;
+    setTimeout(() => {
+      this.position.y -= 2;
+    }, 100);
+  }
+
+  basketColor(multiplier) {
+    multiplier += 0.1;
+    85, 245, 32;
+    let color = "rgb(0,0,0)";
+    if (multiplier <= 1) {
+      color = "rgb( " + 85 * multiplier + ", " + 245 + ", 32)";
+    } else if (multiplier <= 10) {
+      color = "rgb( " + 85 + 20 * multiplier + ", " + 245 + ", 32)";
+    } else {
+      color = "rgb(245, 64, 32)";
     }
-    
-    basketColor(multiplier){
-        multiplier += 0.1;
-        let redColor = 150 * multiplier;
-        let greenColor = 150 / multiplier;
-        let color = 'rgb( ' + redColor + ', '+ greenColor +', 5)';
-        return color
-    }
+    return color;
+  }
 }
